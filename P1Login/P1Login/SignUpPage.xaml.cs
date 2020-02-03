07c5d08a5099c0ce0d98e2b5a47e0e20f2b99fac
Daiivasq <daihianav@gmail.com>
2020-02-03 08:31:27 -0400

    VERSION #1. 8:31 , 2020-02-03.

---
+++ P1Login/P1Login/SignUpPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace P1Login
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void ButtonSignUp_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(UserSignUp.Text) && String.IsNullOrEmpty(PassWordSignUp.Text))
+            {
+                await DisplayAlert("ALERT!", "THERE ARE EMPTY FIELDS", "OK");
+            }
+            else if (PassWordSignUp.Text.Equals(RepeatPass.Text))
+            {
+                await DisplayAlert("ALERT!", "THE FIELDS ARE NOT EQUAL", "OK");
+
+            }
+           else
+            {
+                await DisplayAlert("WELCOME", " ", "OK");
+            }
+
+            
+        }
+    }
+}
