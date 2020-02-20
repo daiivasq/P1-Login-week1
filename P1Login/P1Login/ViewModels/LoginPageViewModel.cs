1cd91ff73fff4507dc1a738796afdb78b2f94556
Daiivasq <daihianav@gmail.com>
2020-02-20 13:16:46 -0400

    V6, 2020-02-20- 1:16 pm

--- P1Login/P1Login/ViewModels/LoginPageViewModel.cs
+++ P1Login/P1Login/ViewModels/LoginPageViewModel.cs
@@ -11,7 +11,7 @@ namespace P1Login.ViewModels
 {
     public class LoginPageViewModel : INotifyPropertyChanged
     {
-        #region PROP
+        
         public UserModel User { get; set; }
 
         public ICommand ButtonLoginClicked_Command { get; set; }
@@ -19,8 +19,7 @@ namespace P1Login.ViewModels
         public ICommand ButtonEyeClicked_Command { get; set; }
         public ImageSource ImageModel { get; set; }
         public bool IsVisible { get; set; }
-        #endregion
-
+        
 
         public LoginPageViewModel() 
         {
